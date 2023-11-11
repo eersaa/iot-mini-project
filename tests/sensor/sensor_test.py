33d5d0795d660ee7403e2aadf3561f40dcbcb01a
eersaa <eerik.saarinen@gmail.com>
2023-11-11 19:20:11 +0000

    Add test for take_measurement

--- tests/sensor/sensor_test.py
+++ tests/sensor/sensor_test.py
@@ -4,8 +4,7 @@ def test_should_return_sensor_name_when_sensor_is_created():
     sensor = Sensor("temperature")
     assert sensor.name == "temperature"
 
-# def test_should_throw_exception_when_sensor_is_created_without_name():
-#     try:
-#         Sensor()
-#     except Exception as e:
-#         assert str(e) == "__init__() missing 1 required positional argument: 'name'"
+def test_should_return_zero_when_taken_empty_measurement():
+    sensor = Sensor("temperature")
+    sensor.take_measurement()
+    assert sensor.measurement == 0
