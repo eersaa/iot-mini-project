493d72f8ed3504fccf0628eddd089deb59e514b2
eersaa <eerik.saarinen@gmail.com>
2023-11-11 18:29:40 +0000

    Update acceptance test to describe the environment better

--- tests/acceptance_test.py
+++ tests/acceptance_test.py
@@ -1,4 +1,12 @@
-def test_should_return_one_temperature_datapoint():
-    goto.dashboard()
-    dashboard.open("temperatures")
-    assert len(dashboard.temperatures) == 1
+def test_should_return_one_temperature_measurement_when_sent_one_measurement():
+    temperature_sensor = Sensor("temperature")
+    iot_node = IotNode.add(temperature_sensor)
+    public_api_service = PublicAPI().create()
+    public_api_service.start()
+    dashboard = Dashboard().create()
+
+    iot_node.temperature_sensor.take_measurement()
+    iot_node.send_message(public_api_service.address)
+
+    dashboard.show("temperatures")
+    assert len(dashboard.export("temperatures").list()) == 1
