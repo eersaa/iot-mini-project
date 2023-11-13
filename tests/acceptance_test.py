f1701d8c0dc20b8ef97c91a45594b5cec9b8f21f
eersaa <eerik.saarinen@gmail.com>
2023-11-13 17:17:08 +0000

    Add type parameter and rename temperature to value parameter

--- tests/acceptance_test.py
+++ tests/acceptance_test.py
@@ -1,4 +1,6 @@
+# Test setup
+
 # This same test could work against both CoAP API and database.
 def test_should_update_measurement_at_datacollector(datacollection):
-    datacollection.send_updated_measurement(temperature=25.0, destination="datacollector")
-    datacollection.confirm_update(temperature=25.0, source="datacollector")
+    datacollection.send_updated_measurement(type="temperature", value="25.0", destination="datacollector")
+    datacollection.confirm_update(type="temperature", value="25.0", destination="datacollector")
