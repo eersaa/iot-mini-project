57855e03928c09eb701b0bf230cf9254d24dd2e5
eersaa <eerik.saarinen@gmail.com>
2023-11-14 17:33:22 +0000

    Add confirm_showed_sample

--- tests/acceptance_test.py
+++ tests/acceptance_test.py
@@ -2,3 +2,6 @@
 def test_should_successfully_show_one_sample_to_user_sent_from_iot_node():
     iot_system.iot_node.send_measurement_sample(type="temperature", value="25.0")
     confirm_showed_sample(iot_system.show_measurement_sample(type="temperature"), value="25.0")
+
+def confirm_showed_sample(sample, value):
+    assert sample == value
