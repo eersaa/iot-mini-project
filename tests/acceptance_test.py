004913c65eecba75fd00213c81eeab9881ab613a
eersaa <eerik.saarinen@gmail.com>
2023-11-14 17:12:56 +0000

    Transform the first test to perspective of an user

--- tests/acceptance_test.py
+++ tests/acceptance_test.py
@@ -1,6 +1,4 @@
-# Test setup
 
-# This same test could work against both CoAP API and database.
-def test_should_update_measurement_at_datacollector(datacollection):
-    datacollection.send_updated_measurement(type="temperature", value="25.0", destination="datacollector")
-    datacollection.confirm_update(type="temperature", value="25.0", destination="datacollector")
+def test_should_successfully_show_one_sample_to_user_sent_from_iot_node():
+    iot_system.iot_node.send_measurement_sample(type="temperature", value="25.0")
+    confirm_showed_sample(iot_system.show_measurement_sample(type="temperature"), value="25.0")
