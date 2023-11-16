525f6097e1c6472f98e76ad7850a2a7d50307e18
eersaa <eerik.saarinen@gmail.com>
2023-11-16 18:20:50 +0000

    Create some skeleton of client

--- tests/coap_api_protocol_driver.py
+++ tests/coap_api_protocol_driver.py
@@ -1,7 +1,18 @@
+import asyncio
+import aiocoap import Context, Message, GET, POST
 
-class CoapApiProtocolDriver:
 
-    def show_measurement_sample(self, measurement_type):
+class CoapApiProtocolDriver(Context, Message):
+    async def __init__(self):
+        protocol = await Context.create_client_context()
+
+    async def show_measurement_sample(self, measurement_type):
+        request = Message(code=GET, uri="coap://localhost:5683/" + measurement_type)
+
+        try:
+            response = await self.protocol.request(request).response
+        except Exception as e:
+            sys.exit(1)
         pass
     
     def send_measurement_sample(self, measurement_type, measurement_value):
