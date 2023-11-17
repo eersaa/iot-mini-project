a31ddac3fb737af06c4e748c73a88a325df00183
eersaa <eerik.saarinen@gmail.com>
2023-11-17 08:11:55 +0000

    Use unittest for test setup

--- tests/acceptance_test.py
+++ tests/acceptance_test.py
@@ -1,7 +1,14 @@
+import unittest
+import iot_system_dsl
 
-def test_should_successfully_show_one_sample_to_user_sent_from_iot_node():
-    iot_system.iot_node.send_measurement_sample(type="temperature", value="25.0")
-    confirm_showed_sample(iot_system.show_measurement_sample(type="temperature"), value="25.0")
+class IotSystemCoapApiAcceptanceTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.iot_node = iot_system_dsl.IoTNode()
+        self.iot_system = iot_system_dsl.IoTSystem(self.iot_node)
 
-def confirm_showed_sample(sample, value):
-    assert sample == value
+    def test_should_successfully_show_one_sample_to_user_sent_from_iot_node(self):
+        self.iot_system.iot_node.send_measurement_sample(type="temperature", value="25.0")
+        self.confirm_showed_sample(self.iot_system.show_measurement_sample(type="temperature"), value="25.0")
+
+    def confirm_showed_sample(self, sample, value):
+        self.assertEqual(sample == value)
