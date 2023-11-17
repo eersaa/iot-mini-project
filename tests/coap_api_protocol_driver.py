26a5b47c69223dd7398874891c3623cbce5e14ff
eersaa <eerik.saarinen@gmail.com>
2023-11-17 11:41:34 +0000

    Don't retransmit request

--- tests/coap_api_protocol_driver.py
+++ tests/coap_api_protocol_driver.py
@@ -5,6 +5,10 @@ from aiocoap import *
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+class TransportTuner(TransportTuning):
+    def __init__(self):
+        super().__init__()
+        self.MAX_RETRANSMIT = 0
 
 class CoapApiProtocolDriver(Context, Message):
     def __init__(self):
@@ -12,7 +16,7 @@ class CoapApiProtocolDriver(Context, Message):
         self.client = self.loop.run_until_complete(Context.create_client_context())
 
     def show_measurement_sample(self, measurement_type):
-        request = Message(code=GET, uri="coap://localhost:5683/" + measurement_type)
+        request = Message(code=GET, uri="coap://localhost:5683/" + measurement_type, transport_tuning=TransportTuner())
         return self.loop.run_until_complete(self.fetch_resource(request))
 
     async def fetch_resource(self, request):
