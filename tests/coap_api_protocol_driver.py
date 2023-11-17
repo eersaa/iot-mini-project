210ea70b40a507ee0e2f055c83e9c4e50fb1adb3
eersaa <eerik.saarinen@gmail.com>
2023-11-17 07:48:18 +0000

    Complete first version of GET test

--- tests/coap_api_protocol_driver.py
+++ tests/coap_api_protocol_driver.py
@@ -1,19 +1,22 @@
 import asyncio
-import aiocoap import Context, Message, GET, POST
+import logging
+from aiocoap import *
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class CoapApiProtocolDriver(Context, Message):
     async def __init__(self):
-        protocol = await Context.create_client_context()
+        self.protocol = await Context.create_client_context()
 
     async def show_measurement_sample(self, measurement_type):
-        request = Message(code=GET, uri="coap://localhost:5683/" + measurement_type)
+        request = Message(code=GET, uri="coap://datacollector:5683/" + measurement_type)
 
         try:
             response = await self.protocol.request(request).response
         except Exception as e:
-            sys.exit(1)
-        pass
+            logger.error(f"Failed to fetch resource: {e} with response code {response.code}")
     
     def send_measurement_sample(self, measurement_type, measurement_value):
         pass
