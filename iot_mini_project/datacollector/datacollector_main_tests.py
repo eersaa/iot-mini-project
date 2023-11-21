1d83c84d01a5bb7c704e7b6b27e614ef198b93e0
eersaa <eerik.saarinen@gmail.com>
2023-11-21 19:27:33 +0000

    Use local discovery because of docker configuration

--- iot_mini_project/datacollector/datacollector_main_tests.py
+++ iot_mini_project/datacollector/datacollector_main_tests.py
@@ -5,6 +5,6 @@ def at_least_one_test_failed(test_result):
     return not test_result.wasSuccessful()
 
 if __name__ == '__main__':
-    tests = unittest.TestLoader().discover('iot_mini_project.datacollector', pattern='*_tests*')
+    tests = unittest.TestLoader().discover('.', pattern='*_tests*')
     test_result = unittest.TextTestRunner().run(tests)
     sys.exit(at_least_one_test_failed(test_result))
