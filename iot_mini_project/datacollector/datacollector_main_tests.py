caa051a9d35cd52d7247a98a950b2c895122a06a
eersaa <eerik.saarinen@gmail.com>
2023-11-21 19:15:52 +0000

    Run all tests for datacollector with this

---
+++ iot_mini_project/datacollector/datacollector_main_tests.py
@@ -0,0 +1,10 @@
+import unittest
+import sys
+
+def at_least_one_test_failed(test_result):
+    return not test_result.wasSuccessful()
+
+if __name__ == '__main__':
+    tests = unittest.TestLoader().discover('iot_mini_project.datacollector', pattern='*_tests*')
+    test_result = unittest.TextTestRunner().run(tests)
+    sys.exit(at_least_one_test_failed(test_result))
