345826cf299146e5be5533a047b11044101b5163
eersaa <eerik.saarinen@gmail.com>
2023-11-23 16:42:17 +0000

    Fix ipv6 address format and add ipv4 address test

--- iot_mini_project/util/util_tests.py
+++ iot_mini_project/util/util_tests.py
@@ -10,7 +10,10 @@ class CoapUriCreatorTest(unittest.TestCase):
         self.assertEqual(create_coap_uri(resource_path="temperature"), "coap://localhost:5683/temperature")
 
     def test_should_use_given_ipv6_address(self):
-        self.assertEqual(create_coap_uri(address="2001:0db8:85a3:0000:0000:8a2e:0370:7334"), "coap://2001:0db8:85a3:0000:0000:8a2e:0370:7334:5683/.well-known/core")
+        self.assertEqual(create_coap_uri(address="[2001:0db8:85a3:0000:0000:8a2e:0370:7334]"), "coap://[2001:0db8:85a3:0000:0000:8a2e:0370:7334]:5683/.well-known/core")
+
+    def test_should_use_given_ipv4_address(self):
+        self.assertEqual(create_coap_uri(address="127.0.0.1"), "coap://127.0.0.1:5683/.well-known/core")
         
 if __name__ == '__main__':
     unittest.main()
