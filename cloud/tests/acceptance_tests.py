f0d2e169c82b6e12872f95b758e33630fa78f6b0
eersaa <eerik.saarinen@gmail.com>
2023-11-23 18:42:25 +0000

    Check if no tests run

--- cloud/tests/acceptance_tests.py
+++ cloud/tests/acceptance_tests.py
@@ -19,7 +19,10 @@ class IotSystemCoapApiAcceptanceTest(unittest.TestCase):
 def at_least_one_test_failed(test_result):
     return not test_result.wasSuccessful()
 
+def no_tests_run(test_result):
+    return test_result.testsRun == 0
+
 if __name__ == '__main__':
     acceptance_tests = unittest.TestLoader().discover('.', pattern='acceptance_tests*')
     test_result = unittest.TextTestRunner(verbosity=2).run(acceptance_tests)
-    sys.exit(at_least_one_test_failed(test_result))
+    sys.exit(at_least_one_test_failed(test_result) or no_tests_run(test_result))
