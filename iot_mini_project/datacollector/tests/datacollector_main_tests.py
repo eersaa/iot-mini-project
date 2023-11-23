d4dab9b00cff1d23176f739cb2d1a7c2e28be3bb
eersaa <eerik.saarinen@gmail.com>
2023-11-23 13:45:40 +0000

    Validate that tests were run

--- iot_mini_project/datacollector/tests/datacollector_main_tests.py
+++ iot_mini_project/datacollector/tests/datacollector_main_tests.py
@@ -4,7 +4,10 @@ import sys
 def at_least_one_test_failed(test_result):
     return not test_result.wasSuccessful()
 
+def no_tests_run(test_result):
+    return test_result.testsRun == 0
+
 if __name__ == '__main__':
     tests = unittest.TestLoader().discover('.', pattern='*_tests*')
     test_result = unittest.TextTestRunner().run(tests)
-    sys.exit(at_least_one_test_failed(test_result))
+    sys.exit(at_least_one_test_failed(test_result) or no_tests_run(test_result))
