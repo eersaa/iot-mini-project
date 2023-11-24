fda0d0f183a934a01995f635234b11cee4908251
eersaa <eerik.saarinen@gmail.com>
2023-11-24 13:43:41 +0000

    Use unittest.main() to run just this script

--- cloud/tests/acceptance/acceptance_tests.py
+++ cloud/tests/acceptance/acceptance_tests.py
@@ -23,6 +23,4 @@ def no_tests_run(test_result):
     return test_result.testsRun == 0
 
 if __name__ == '__main__':
-    acceptance_tests = unittest.TestLoader().discover('.', pattern='acceptance_tests*')
-    test_result = unittest.TextTestRunner(verbosity=2).run(acceptance_tests)
-    sys.exit(at_least_one_test_failed(test_result) or no_tests_run(test_result))
+    unittest.main()
