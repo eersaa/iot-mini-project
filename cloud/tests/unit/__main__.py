1b3df826a221108e0c71fccfaa55019d532a1b53
eersaa <eerik.saarinen@gmail.com>
2023-11-24 13:22:13 +0000

    Make tests.unit to executable package

---
+++ cloud/tests/unit/__main__.py
@@ -0,0 +1,13 @@
+import unittest
+import sys
+
+def at_least_one_test_failed(test_result):
+    return not test_result.wasSuccessful()
+
+def no_tests_run(test_result):
+    return test_result.testsRun == 0
+
+if __name__ == '__main__':
+    tests = unittest.TestLoader().discover('tests.unit', pattern='*_tests*')
+    test_result = unittest.TextTestRunner().run(tests)
+    sys.exit(at_least_one_test_failed(test_result) or no_tests_run(test_result))
