79b93536126ed45352004bbf93906b91dc58b1ba
eersaa <eerik.saarinen@gmail.com>
2023-11-24 13:46:04 +0000

    Look for different pattern

--- cloud/tests/acceptance/__main__.py
+++ cloud/tests/acceptance/__main__.py
@@ -4,7 +4,7 @@ import unittest
 from tests.util.unittest_helpers import at_least_one_test_failed, no_tests_run
 
 def main():
-    acceptance_tests = unittest.TestLoader().discover('tests.acceptance', pattern='*acceptance_tests*')
+    acceptance_tests = unittest.TestLoader().discover('tests.acceptance', pattern='*_tests*')
     test_result = unittest.TextTestRunner(verbosity=2).run(acceptance_tests)
     sys.exit(at_least_one_test_failed(test_result) or no_tests_run(test_result))
 
